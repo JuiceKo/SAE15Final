bb8c8a9297451f170bab5350197a2db88a0a8755
JuiceKo <94040097+JuiceKo@users.noreply.github.com>
2022-01-28 09:56:27 +0100

    Delete netflix/data/processed/Programme Test Dectection France directory

--- netflix/data/processed/Programme Test Dectection France/France.py
+++
@@ -1,8 +0,0 @@
-import csv
-counter = 0
-word_to_search = 'france'
-with open('merged.csv', encoding='UTF-8') as f:
-    for line in csv.reader(f):
-         if any(word_to_search in l for l in map(str.lower, line)):
-              counter += 1
-print(counter)
