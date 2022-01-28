a5bf77918aa36122920e891a3cc6f5ce8f9972ee
ja1met0ut <94043754+ja1met0ut@users.noreply.github.com>
2022-01-28 11:26:59 +0100

    Add files via upload

---
+++ netflix/data/processed/france-finale/FranceSecondeWay.py
@@ -0,0 +1,12 @@
+import csv
+counter = 0
+word_to_search = 'Movie'
+with open('merged.csv', encoding='UTF-8') as f:
+    for line in csv.reader(f):
+        if any(word_to_search in l for l in map(str.lower, line)):
+            word_to_search = 'france'
+            with open('merged.csv', encoding='UTF-8') as f:
+                for line in csv.reader(f):
+                        if any(word_to_search in l for l in map(str.lower, line)):
+                            counter += 1
+print(counter)
